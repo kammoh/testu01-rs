4befb8bdd95aecc0997dc5e285636801faf75778
Loïc Damien <loic.damien@dzamlo.ch>
2015-02-10 00:51:32 +0100

    Add license

--- examples/small_crush_xorshift.rs
+++ examples/small_crush_xorshift.rs
@@ -1,3 +1,19 @@
+// A rust wrapper to a small subset of TestU01 (http://simul.iro.umontreal.ca/testu01/tu01.html).
+// Copyright (C) 2015  Loïc Damien
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #![feature(std_misc)]
 
 extern crate testu01;
